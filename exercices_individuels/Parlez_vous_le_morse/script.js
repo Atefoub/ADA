252e4ab7a71791251a4b8a223d18eba0c029cb80
Antoine Mourin <antoinem1pro@gmail.com>
2025-10-20 15:49:12 +0200

    suite

--- exercices_individuels/Parlez_vous_le_morse/script.js
+++ exercices_individuels/Parlez_vous_le_morse/script.js
@@ -1,45 +1,44 @@
 // Etape 1 : fonction getLatinCharacterList
 
-
 const latinToMorse = {
-	'A':'.-',
-	'B':'-...',
-	'C':'-.-.',
-	'D':'-..',
-	'E':'.',
-	'F':'..-.',
-	'G':'--.',
-	'H':'....',
-	'I':'..',
-	'J':'.---',
-	'K':'-.-',
-	'L':'.-..',
-	'M':'--',
-	'N':'-.',
-	'O':'---',
-	'P':'.--.',
-	'Q':'--.-',
-	'R':'.-.',
-	'S':'...',
-	'T':'-',
-	'U':'..-',
-	'V':'...-',
-	'W':'.--',
-	'X':'-..-',
-	'Y':'-.--',
-	'Z':'--..'
+  A: ".-",
+  B: "-...",
+  C: "-.-.",
+  D: "-..",
+  E: ".",
+  F: "..-.",
+  G: "--.",
+  H: "....",
+  I: "..",
+  J: ".---",
+  K: "-.-",
+  L: ".-..",
+  M: "--",
+  N: "-.",
+  O: "---",
+  P: ".--.",
+  Q: "--.-",
+  R: ".-.",
+  S: "...",
+  T: "-",
+  U: "..-",
+  V: "...-",
+  W: ".--",
+  X: "-..-",
+  Y: "-.--",
+  Z: "--..",
+};
+
+function getLatinCharacterList(text) {
+  return text.split("");
 }
 
-function getLatinCharacterList (text){
-  return text.split("")
-}
-
-console.log("getLatinCharacterList", getLatinCharacterList("hello, world"))
+console.log("getLatinCharacterList", getLatinCharacterList("hello, world"));
 
 // Etape 2 : fonction translateLatinCharacter
 
-function translateLatinCharacter(character){
-    return latinToMorse[character.toUpperCase()]
+function translateLatinCharacter(character) {
+  return latinToMorse[character.toUpperCase()];
 }
 
-console.log("translateLatinCharacter",translateLatinCharacter("a"))
+console.log("translateLatinCharacter", translateLatinCharacter("a"));
