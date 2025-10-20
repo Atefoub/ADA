927892cecc9bcc56e1705e31a36047dc98d27233
Antoine Mourin <antoinem1pro@gmail.com>
2025-10-20 15:43:34 +0200

    suite

--- exercices_individuels/ma_page_web/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const bouton = document.getElementById("btn-exercices");
-  const sectionExercices = document.getElementById("exercices");
-
-  bouton.addEventListener("click", function (e) {
-    e.preventDefault(); // Empêche le saut brutal
-    sectionExercices.scrollIntoView({ behavior: "smooth" });
-  });
-});
