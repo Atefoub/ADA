dbf9ba9d8c5fb9ab2ceb48ea8cab190ff078bbcf
Antoine Mourin <antoinem1pro@gmail.com>
2025-10-20 15:31:59 +0200

    mon portfolio

--- exercices_individuels/Parlez_vous_le_morse/script.js
+++ exercices_individuels/Parlez_vous_le_morse/script.js
@@ -1,4 +1,45 @@
-// https://www.youtube.com/watch?v=eI4J0WwECjk 25 minutes
+// Etape 1 : fonction getLatinCharacterList
 
-console.log ("hello")
 
+const latinToMorse = {
+	'A':'.-',
+	'B':'-...',
+	'C':'-.-.',
+	'D':'-..',
+	'E':'.',
+	'F':'..-.',
+	'G':'--.',
+	'H':'....',
+	'I':'..',
+	'J':'.---',
+	'K':'-.-',
+	'L':'.-..',
+	'M':'--',
+	'N':'-.',
+	'O':'---',
+	'P':'.--.',
+	'Q':'--.-',
+	'R':'.-.',
+	'S':'...',
+	'T':'-',
+	'U':'..-',
+	'V':'...-',
+	'W':'.--',
+	'X':'-..-',
+	'Y':'-.--',
+	'Z':'--..'
+}
+
+function getLatinCharacterList (text){
+  return text.split("")
+}
+
+console.log("getLatinCharacterList", getLatinCharacterList("hello, world"))
+
+// Etape 2 : fonction translateLatinCharacter
+
+function translateLatinCharacter(character){
+    return latinToMorse[character.toUpperCase()]
+}
+
+console.log("translateLatinCharacter",translateLatinCharacter("a"))
