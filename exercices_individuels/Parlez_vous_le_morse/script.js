e9f4a9660b6820edcc356bcb48720c92112cb148
Antoine Mourin <antoinem1pro@gmail.com>
2025-10-22 15:05:45 +0200

    Update script.js

--- exercices_individuels/Parlez_vous_le_morse/script.js
+++ exercices_individuels/Parlez_vous_le_morse/script.js
@@ -49,16 +49,23 @@ console.log("translateLatinCharacter", translateLatinCharacter("a"));
 // Attention: la table de correspondance ne contient que des caractères majuscules.
 
 function encode(text) {
-  const characters = getLatinCharacterList(text);
-  const morseList = characters.map(char => translateLatinCharacter(char));
-  return morseList.filter(Boolean).join(" ");
+    const array = []; // faire un tableau vide pour stocker le résultat
+    const splittedText = getLatinCharacterList(text); // stocker le texte séparé grâce à la fonction dans une variable
+    for (let i = 0; i < splittedText.length; i++) { // boucler sur le texte
+        if (splittedText[i] == " ") { // gérer le cas où le caractère est un espace
+            array.push("/"); // si c'en est un, alors mettre un / dans le tableau "array" pour représenter l'espace
+        } else { // si non, traduire le texte directement en morse grâce à la fonction et push le morse dans le tableau
+            array.push(translateLatinCharacter(splittedText[i]));
+        }
+    }
+    return array.join(" "); // transformer le tableau en chaine de caractères
 }
 
-console.log("encode", encode("hello, world"));
-
-
+console.log(encode("Ceci est un test"));
 
 // Étape 4
-// Vous trouverez en annexe 2 le dictionnaire de correspondance inversé. Ajoutez-le à votre code, et appliquez les procédés similaire à ce qui a été fait pour le encode pour créer une fonction decode. Dans cet exercice, on admettra que les lettres en morse sont séparées par un espace, et les mots par des “/” (slash).
+// Vous trouverez en annexe 2 le dictionnaire de correspondance inversé. Ajoutez-le à votre code,
+// et appliquez les procédés similaire à ce qui a été fait pour le encode pour créer une fonction decode.
+// Dans cet exercice, on admettra que les lettres en morse sont séparées par un espace, et les mots par des “/” (slash).
 
 // Ainsi, créer la fonction getMorseCharacterList ainsi que translateMorseCharacter.
